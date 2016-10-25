715defdd16ec82795d66b1bc60866d1033fd0efe
Murrah <JMMURRAH@eprod.com>
2016-10-25 14:00:26 -0500

    added missing convertfrom-configdata function and some debugging output.

--- build.ps1
+++ build.ps1
@@ -1,9 +1,27 @@
 Param(
     $ResourceGroupName,
     $ConfigurationName,
-    $SourcePath
+    $SourceRoot
 )
 
+function ConvertFrom-ConfigData
+{
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [Microsoft.PowerShell.DesiredStateConfiguration.ArgumentToConfigurationDataTransformation()]
+        [hashtable] $data
+    )
+    return $data
+}
+
+get-item
+get-childitem
+pwd
+write-output "resourcegroupname = $resourcegroupname"
+write-output "configurationname = $ConfigurationName"
+write-output "sourceroot = $sourceroot"
+
 $automationaccount = get-azurermautomationaccount -ResourceGroupName $ResourceGroupName
-Import-AzureRmAutomationDscConfiguration -ResourceGroupName $ResourceGroupName -AutomationAccountName $automationaccount.AutomationAccountName  -SourcePath "$SourcePath$ConfigurationName.ps1" -Published
-Start-AzureRmAutomationDscCompilationJob -ConfigurationName $ConfigurationName -ConfigurationData $(ConvertFrom-ConfigData "$SourcePath`ConfigurationData.psd1") -ResourceGroupName $ResourceGroupName -AutomationAccountName $automationaccount.AutomationAccountName
+Import-AzureRmAutomationDscConfiguration -ResourceGroupName $ResourceGroupName -AutomationAccountName $automationaccount.AutomationAccountName  -SourcePath "$SourceRoot$ConfigurationName.ps1" -Published
+Start-AzureRmAutomationDscCompilationJob -ConfigurationName $ConfigurationName -ConfigurationData $(ConvertFrom-ConfigData "$SourceRoot`ConfigurationData.psd1") -ResourceGroupName $ResourceGroupName -AutomationAccountName $automationaccount.AutomationAccountName
