2acde6af216713a66fc862ec1e12073f7d47a463
Murrah <JMMURRAH@eprod.com>
2016-10-25 14:25:21 -0500

    error in debugging command

--- build.ps1
+++ build.ps1
@@ -13,13 +13,13 @@ function ConvertFrom-ConfigData
     )
     return $data
 }
+get-location
+get-item -path .
+get-childitem -path .
 
-get-item
-get-childitem
-pwd
 write-output "resourcegroupname = $resourcegroupname"
 write-output "configurationname = $ConfigurationName"
-write-output "sourceroot = $sourceroot"
+#write-output "sourceroot = $sourceroot"
 
 $automationaccount = get-azurermautomationaccount -ResourceGroupName $ResourceGroupName
 Import-AzureRmAutomationDscConfiguration -ResourceGroupName $ResourceGroupName -AutomationAccountName $automationaccount.AutomationAccountName  -SourcePath ".\$ConfigurationName.ps1" -Published
