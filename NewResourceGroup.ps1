0f427c4343737f6b7fd084cb09758699da7e009f
Murrah <JMMURRAH@eprod.com>
2016-10-27 10:49:29 -0500

    added location parameter to new resource group

--- NewResourceGroup.ps1
+++ NewResourceGroup.ps1
@@ -1,8 +1,8 @@
 #First build step, creates resource group if it doesn't already exist
 
-Param($ResourceGroupName)
+Param($ResourceGroupName, $location)
 
 If (!(Get-AzureRmResourceGroup $ResourceGroupName -ea 'SilentlyContinue')){
-    New-AzureRmResourceGroup $ResourceGroupName
+    New-AzureRmResourceGroup -name $ResourceGroupName -location $location
 }
 
