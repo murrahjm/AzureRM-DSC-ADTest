781d491a1cf269a0a2e7d40e7211ec1e0cb97bc2
Murrah <JMMURRAH@eprod.com>
2016-11-01 10:54:59 -0500

    new template azuredeploy to combine vm creation with dsc module setup and extension configuration

--- NewResourceGroup.ps1
+++ NewResourceGroup.ps1
@@ -5,4 +5,7 @@ Param($ResourceGroupName, $location)
 If (!(Get-AzureRmResourceGroup $ResourceGroupName -ea 'SilentlyContinue')){
     New-AzureRmResourceGroup -name $ResourceGroupName -location $location
 }
+If (!(Get-AzureRMAutomationAccount -ResourceGroupName $ResourceGroupName -ea 'SilentlyContinue')){
+    New-AzureRMAutomationAccount -name "AzureAutomation-$ResourceGroupName" -Location $location -Plan Free
+}
 
