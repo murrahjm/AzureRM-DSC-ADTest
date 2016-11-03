e145307b61e13d8883db319a8d0a829952e8f62c
Murrah <JMMURRAH@eprod.com>
2016-11-03 10:43:43 -0500

    simplifying test dsc config to isolate registration errors.

--- configdatatest.psd1
+++ configdatatest.psd1
@@ -2,9 +2,6 @@
     AllNodes = @(
 		@{
 			NodeName = 'DC1'
-			Domain = 'testdomain.local'
-			Site = 'Site1'
-			Functions = @('DomainController','SchemaMaster','DomainNamingMaster','RIDMaster','PDCEmulator','InfrastructureMaster')
 			}
     )
 }
--- testconfig.ps1
+++ testconfig.ps1
@@ -1,7 +1,5 @@
 configuration testconfig {
 	Import-DscResource -modulename PSDesiredStateConfiguration
-	Import-DscResource -modulename xPSDesiredStateConfiguration -moduleversion 4.0.0.0
-	Import-DscResource -modulename xActiveDirectory
     node $allnodes.NodeName {
         
         windowsfeature telnetclient {
