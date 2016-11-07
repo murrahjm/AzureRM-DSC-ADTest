5ac24058a19f3dc3218f6ff2b02911eff7f2c2f1
Murrah <JMMURRAH@eprod.com>
2016-11-07 09:46:14 -0600

    type in dscreginfo variable reference

--- NewResourceGroup.ps1
+++ NewResourceGroup.ps1
@@ -23,8 +23,8 @@ $DSCRegInfo = Get-AzureRmAutomationRegistrationInfo -ResourceGroupName $env:Reso
 $Configdata = $(ConvertFrom-ConfigData "$env:BUILD_SOURCESDIRECTORY\configurationdata.psd1" | convertto-json -Depth 20 -Compress).ToString()
 $TimeStamp = $(get-date -uformat "%D %r").ToString()
 
-write-host "##vso[task.setvariable variable=DSCRegistrationKey;]$(DSCRegInfo.PrimaryKey)"
-write-host "##vso[task.setvariable variable=DSCRegistrationURL;]$(DSCRegInfo.EndPoint)"
+write-host "##vso[task.setvariable variable=DSCRegistrationKey;]$($DSCRegInfo.PrimaryKey)"
+write-host "##vso[task.setvariable variable=DSCRegistrationURL;]$($DSCRegInfo.EndPoint)"
 write-host "##vso[task.setvariable variable=jobID;]$([System.Guid]::NewGuid().toString())"
 write-host "##vso[task.setvariable variable=ConfigData;]$Configdata"
 write-host "##vso[task.setvariable variable=timestamp;]$TimeStamp"
