f545abc84b1a178b97fb36b3acd19d6e1d627ea2
Murrah <JMMURRAH@eprod.com>
2016-11-07 10:50:22 -0600

    reformatted dscregistrationkey value as secure string

--- NewResourceGroup.ps1
+++ NewResourceGroup.ps1
@@ -22,8 +22,9 @@ If (!(Get-AzureRMAutomationAccount -ResourceGroupName $env:ResourceGroupName -ea
 $DSCRegInfo = Get-AzureRmAutomationRegistrationInfo -ResourceGroupName $env:ResourceGroupName -AutomationAccountName $AZureAutomationAccountName
 $Configdata = $(ConvertFrom-ConfigData "$env:BUILD_SOURCESDIRECTORY\configurationdata.psd1" | convertto-json -Depth 20 -Compress).ToString()
 $TimeStamp = $(get-date -uformat "%D %r").ToString()
-
-write-host "##vso[task.setvariable variable=DSCRegistrationKey;]$($DSCRegInfo.PrimaryKey)"
+$DSCRegPrimaryKey = $($DSCRegInfo.PrimaryKey | ConvertTo-SecureString -AsPlainText -Force).ToString()
+ 
+write-host "##vso[task.setvariable variable=DSCRegistrationKey;]$DSCRegPrimaryKey"
 write-host "##vso[task.setvariable variable=DSCRegistrationURL;]$($DSCRegInfo.EndPoint)"
 write-host "##vso[task.setvariable variable=jobID;]$([System.Guid]::NewGuid().toString())"
 write-host "##vso[task.setvariable variable=ConfigData;]`'$Configdata`'"
