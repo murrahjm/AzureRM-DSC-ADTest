f1758ef44fa7d0aae36dabc2db3b205b734241f3
Murrah <JMMURRAH@eprod.com>
2016-11-07 11:01:41 -0600

    cannot pass registration key as secure string between build tasks.  changing to string value.

--- NewResourceGroup.ps1
+++ NewResourceGroup.ps1
@@ -23,7 +23,7 @@ $DSCRegInfo = Get-AzureRmAutomationRegistrationInfo -ResourceGroupName $env:Reso
 $Configdata = $(ConvertFrom-ConfigData "$env:BUILD_SOURCESDIRECTORY\configurationdata.psd1" | convertto-json -Depth 20 -Compress).ToString()
 $TimeStamp = $(get-date -uformat "%D %r").ToString()
  
-write-host "##vso[task.setvariable variable=DSCRegistrationKey;issecret=true;]$($DSCRegInfo.PrimaryKey)"
+write-host "##vso[task.setvariable variable=DSCRegistrationKey;]$($DSCRegInfo.PrimaryKey)"
 write-host "##vso[task.setvariable variable=DSCRegistrationURL;]$($DSCRegInfo.EndPoint)"
 write-host "##vso[task.setvariable variable=jobID;]$([System.Guid]::NewGuid().toString())"
 write-host "##vso[task.setvariable variable=ConfigData;]`'$Configdata`'"
