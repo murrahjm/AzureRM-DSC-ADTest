c13167403c7c32704f7dad18fc14f20eeae3f8c7
Murrah <JMMURRAH@eprod.com>
2016-11-07 09:41:49 -0600

    corrected syntax in vso output to set build variables.

--- NewResourceGroup.ps1
+++ NewResourceGroup.ps1
@@ -23,9 +23,9 @@ $DSCRegInfo = Get-AzureRmAutomationRegistrationInfo -ResourceGroupName $env:Reso
 $Configdata = $(ConvertFrom-ConfigData "$env:BUILD_SOURCESDIRECTORY\configurationdata.psd1" | convertto-json -Depth 20 -Compress).ToString()
 $TimeStamp = $(get-date -uformat "%D %r").ToString()
 
-##vso[task.setvariable variable=DSCRegistrationKey;]$(DSCRegInfo.PrimaryKey)
-##vso[task.setvariable variable=DSCRegistrationURL;]$(DSCRegInfo.EndPoint)
-##vso[task.setvariable variable=jobID;]$([System.Guid]::NewGuid().toString())
-##vso[task.setvariable variable=ConfigData;]$Configdata
-##vso[task.setvariable variable=timestamp;]$TimeStamp
+write-host "##vso[task.setvariable variable=DSCRegistrationKey;]$(DSCRegInfo.PrimaryKey)"
+write-host "##vso[task.setvariable variable=DSCRegistrationURL;]$(DSCRegInfo.EndPoint)"
+write-host "##vso[task.setvariable variable=jobID;]$([System.Guid]::NewGuid().toString())"
+write-host "##vso[task.setvariable variable=ConfigData;]$Configdata"
+write-host "##vso[task.setvariable variable=timestamp;]$TimeStamp"
 
