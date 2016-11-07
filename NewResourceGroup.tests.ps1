51ad6f955d4371b37d7a25da04f29f90b142b644
Murrah <JMMURRAH@eprod.com>
2016-11-07 08:06:11 -0600

    updating resourcegroup variable to reference name passed through build tasks

--- NewResourceGroup.tests.ps1
+++ NewResourceGroup.tests.ps1
@@ -1,15 +1,14 @@
 #pester test to validate resource group exists before continuing to next build step
-Param($ResourceGroupname)
-    Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
 
 Describe "Resource Group Test"{
     It 'verifies Resourcegroup creation'{
-        $ResourceGroup = get-AzureRMResourceGroup -Name $ResourceGroupname -ea Stop
-        $ResourceGroup.ResourceGroupName | should be $ResourceGroupName             
+        $ResourceGroup = get-AzureRMResourceGroup -Name $env:ResourceGroupname -ea Stop
+        $ResourceGroup.ResourceGroupName | should be $env:ResourceGroupName             
         $ResourceGroup.ProvisioningState | should be 'Succeeded'
     }
     It 'verifies azure automation account creation'
-        $AA = Get-AzureRmAutomationAccount -ResourceGroupName $ResourceGroupname -Name "AzureAutomation-$ResourceGroupname" -ea Stop
+        $AA = Get-AzureRmAutomationAccount -ResourceGroupName $env:ResourceGroupname -Name "AzureAutomation-$env:ResourceGroupname" -ea Stop
         $AA | should not benullorempty
     }
     It 'verifies TFS variables set with DSC info' {
